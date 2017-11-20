547921cc69357fb6fc8121f095740b7f45d5ddf1
Dean Brophy <dbrophy@ihsmarkit.com>
2017-11-20 12:04:22 -0700

    Tabs Working

--- src/TwTabs.jsx
+++ src/TwTabs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Divider, Subheader, FlatButton } from 'material-ui';
+import PropTypes from 'prop-types';
 
 const tabContainerStyle = {
   display: 'flex',
@@ -47,32 +48,91 @@ const buttonStyle = {
   padding: '8px',
 };
 
-export const TwTabs = (props) => {
-  const selected = props
-    .children
-    .find(f => f.props.selected);
+export class TwTabs extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedIndex: 0,
+    };
+
+    this.handleTabClick = this
+      .handleTabClick
+      .bind(this);
+  }
+
+  handleTabClick(selectedIndex) {
+    this.setState({ selectedIndex });
+  }
+
+  render() {
+    const mappedChildren = this
+      .props
+      .children
+      .map((v, i) => ({
+        ...v,
+        props: {
+          ...v.props,
+          index: i,
+          selected: i === this.state.selectedIndex,
+          onClick: this.handleTabClick,
+        },
+      }));
+
+
+    const selected = mappedChildren
+      .find(v => v.props.selected);
+
+    console.log({ mappedChildren });
+
+    return (
+      <div style={outerContainerStyle}>
+        <div style={tabContainerStyle}>
+          {mappedChildren}
+        </div>
+        <Divider /> {selected.props.children}
+      </div>
+    );
+  }
+}
+
+TwTabs.propTypes = {
+  children: PropTypes.oneOf([
+    PropTypes.node, PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+TwTabs.defaultProps = {
+  children: null,
+};
+
+export const TwTab = (props) => {
+  const handleClick = () => {
+    console.log({ props });
+    props.onClick(props.index);
+  };
 
   return (
-    <div style={outerContainerStyle}>
-      <div style={tabContainerStyle}>
-        {props.children}
-      </div>
-      <Divider /> {selected.props.children}
+    <div style={tabStyle}>
+      <FlatButton style={buttonStyle} onClick={handleClick}>
+        <Subheader style={subheaderStyle}>{props.label}
+        </Subheader>
+        <div style={tabValueStyle}>
+          {props.value}
+        </div>
+      </FlatButton>
+      {props.selected
+        ? <Divider style={dividerStyle} />
+        : null}
     </div>
   );
 };
+TwTab.propTypes = {
+  label: PropTypes.string.isRequired,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
-export const TwTab = props => (
-  <div style={tabStyle}>
-    <FlatButton style={buttonStyle}>
-      <Subheader style={subheaderStyle}>{props.label}
-      </Subheader>
-      <div style={tabValueStyle}>
-        {props.value}
-      </div>
-    </FlatButton>
-    {props.selected
-      ? <Divider style={dividerStyle} />
-      : null}
-  </div>
-);
+TwTab.defaultProps = {
+  selected: false,
+};
